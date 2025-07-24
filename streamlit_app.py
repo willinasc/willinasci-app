7ff58b7524c1c2e72e7827885fb2bd5a9f7dafc7
willinasc <83487282+willinasc@users.noreply.github.com>
2025-07-24 16:46:00 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,4 +16,15 @@ Equipamentos = st.sidebar.selectbox("Equipamentos", df["DESCRIÇÃO"].unique())
 df_filtered = df[df["DESCRIÇÃO"] == Equipamentos]
 st.write(df_filtered)
 
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
+    st.metric("TAG", df_filtered["MANUTENÇÃO"].counter()
+with col2:
+   st.metric("CALIBRAÇÃO", df_filtered["CALIBRAÇÃO"].counter()
+with col3:
+   st.metric("QUALIFICAÇÃO", df_filtered["QUALIFICAÇÃO"].counter()
+with col4:
+    st.metric("MARCA", df_filtered["MARCA"].counter()
+
 
