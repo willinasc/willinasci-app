edcfedfbfc0fe363558d169aa569f6a33f064e3b
willinasc <83487282+willinasc@users.noreply.github.com>
2025-07-24 15:57:49 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,15 +7,15 @@ st.set_page_config(layout="wide")
 st.info(
     "Acompanhamento das Manutenções, Calibrações e Qualificaçoes dos Equipamentos"
 )
-with st.sidebar.title("Menu"):
-    st.selectbox('Selecione', ["Equipamentos","Manutenção","Calibração","Qualificação"])
-    Equipamentos = st.sidebar.selectbox("DESCRIÇÃO", df["DESCRIÇÃO"].unique())
-    df_filtered = df[df["DESCRIÇÃO"] == Equipamentos]
    
 st.file_uploader("Pesquisar arquivo")
 st.divider()
 df = pd.read_csv("equipamentos.csv", sep=";")
 df = df[["TAG", "DESCRIÇÃO", "MARCA", "STATUS DE USO", "MANUTENÇÃO", "CALIBRAÇÃO", "QUALIFICAÇÃO"]]
+with st.sidebar.title("Menu"):
+    st.selectbox('Selecione', ["Equipamentos","Manutenção","Calibração","Qualificação"])
+    Equipamentos = st.sidebar.selectbox("DESCRIÇÃO", df["DESCRIÇÃO"].unique())
+    df_filtered = df[df["DESCRIÇÃO"] == Equipamentos]
 st.write(df)
 
 
