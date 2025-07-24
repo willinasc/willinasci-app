451d2648f0acd526abc3dba4d629389ba18f4ab8
willinasc <83487282+willinasc@users.noreply.github.com>
2025-07-24 10:47:06 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Gestão de Equipamentos Controle de Qualidade")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Acompanhamento das Manutenções, Clibrações e Qualificaçoes dos Equipamentos"
 )
