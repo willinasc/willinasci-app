d626bde1e14041a7effb7b60be28d12c8515dd42
willinasc <83487282+willinasc@users.noreply.github.com>
2025-07-24 16:17:17 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,7 @@ st.info(
 )
 with st.sidebar.title("Menu"):
     st.selectbox('Selecione', ["Equipamentos","Manutenção","Calibração","Qualificação"])
-    month = st.sidebar.selectbox("Mês", df["Month"].unique())
-df_filtered = df[df["Month"] == month]
-       
+           
 st.file_uploader("Pesquisar arquivo")
 st.divider()
 df = pd.read_csv("equipamentos.csv", sep=";")
