d90b6fca8b404ea11cf9ab6cda73d72c2a91b380
willinasc <83487282+willinasc@users.noreply.github.com>
2025-07-24 13:35:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,11 @@ st.info(
     "Acompanhamento das Manutenções, Calibrações e Qualificaçoes dos Equipamentos"
 )
 with st.sidebar.title("Menu"):
-    b1 = st.button("Manutenção")
-print(b1)
+    b1 = st.button("Planilha")
+    b2 = st.button("Manutenção")
+    b3 = st.button("Calibração")
+    b4 = st.button("Qualificação")
+print(b1, b2, b3, b4)
 st.file_uploader("Pesquisar arquivo")
 st.divider()
 
