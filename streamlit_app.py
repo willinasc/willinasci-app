45470e5721559365c23e6b0c5d770b8b3fcbdcfc
willinasc <83487282+willinasc@users.noreply.github.com>
2026-04-28 14:12:37 -0300

    Add seaborn import to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import openpyxl
+import seaborn
 
 st.title("🎈 Gestão de Equipamentos Controle de Qualidade")
 st.set_page_config(layout="wide")
