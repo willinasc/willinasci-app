171d98745f0f6493ce7c1662d2f8f267192c6005
willinasc <83487282+willinasc@users.noreply.github.com>
2026-04-28 14:14:25 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import openpyxl
-import seaborn
+import seaborn as sns
 
 st.title("🎈 Gestão de Equipamentos Controle de Qualidade")
 st.set_page_config(layout="wide")
@@ -15,6 +15,8 @@ arquivo = st.file_uploader("Pesquisar arquivo")
 if arquivo:
     df = pd.read_excel(arquivo, sheet_name='LISTA MESTRA DE EQUIPAMENTOS', engine="openpyxl", nrows=301, usecols="A:W")
     df
+df.groupby('STATUS DE USO').size().plot(kind='barh', color=sns.palettes.mpl_palette('Dark2'))
+plt.gca().spines[['top', 'right',]].set_visible(False)
 
 st.divider()
 df = pd.read_csv("equipamentos.csv", sep=";")
