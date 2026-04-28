a2ab795959bcddae4722a9a557c9709328cdab6c
willinasc <83487282+willinasc@users.noreply.github.com>
2026-04-28 14:17:07 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,15 @@ arquivo = st.file_uploader("Pesquisar arquivo")
 if arquivo:
     df = pd.read_excel(arquivo, sheet_name='LISTA MESTRA DE EQUIPAMENTOS', engine="openpyxl", nrows=301, usecols="A:W")
     df
-df.groupby('STATUS DE USO').size().plot(kind='barh', color=sns.palettes.mpl_palette('Dark2'))
-plt.gca().spines[['top', 'right',]].set_visible(False)
+
 
 st.divider()
+
+df.groupby('STATUS DE USO').size().plot(kind='barh', color=sns.palettes.mpl_palette('Dark2'))
+plt.gca().spines[['top', 'right',]].set_visible(False)
+plt.show()
+
+
 df = pd.read_csv("equipamentos.csv", sep=";")
 df = df[["TAG", "DESCRIÇÃO", "MARCA", "STATUS DE USO", "MANUTENÇÃO", "CALIBRAÇÃO", "QUALIFICAÇÃO"]]
 Equipamentos = st.sidebar.selectbox("Equipamentos", df["DESCRIÇÃO"].unique(), index=None, placeholder="Selecione o equipamento")
